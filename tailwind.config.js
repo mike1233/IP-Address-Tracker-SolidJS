f6ec632433452740a02ae1cc0ba008674ead8b07
Mike Schepers <mike.schepers@infocaster.net>
2023-01-28 16:48:32 +0100

    added font and theme style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Very Dark Gray": "hsl(0, 0%, 17%)",
+        "Dark Gray": "hsl(0, 0%, 59%)",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+      },
+    },
   },
   plugins: [],
 };
